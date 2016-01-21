a642b47f809a13e4632bf474b992b61a830b0800
Matt Todd <matt.c.todd@gmail.com>
2016-01-21 15:25:05 -0600

    tutorial game

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,41 @@
 require 'gosu'
+require_relative './player.rb'
 
-class MyWindow < Gosu::Window
+class GameWindow < Gosu::Window
   def initialize
-    super(640, 480)
-    self.caption = 'Hello World!'
+    super 640, 480
+    self.caption = "Gosu Tutorial Game"
+
+    @background_image = Gosu::Image.new("media/space.png", :tileable => true)
+
+    @player = Player.new
+    @player.warp(320, 240)
+  end
+
+  def update
+    if Gosu::button_down? Gosu::KbLeft or Gosu::button_down? Gosu::GpLeft then
+      @player.turn_left
+    end
+    if Gosu::button_down? Gosu::KbRight or Gosu::button_down? Gosu::GpRight then
+      @player.turn_right
+    end
+    if Gosu::button_down? Gosu::KbUp or Gosu::button_down? Gosu::GpButton0 then
+      @player.accelerate
+    end
+    @player.move
+  end
+
+  def draw
+    @player.draw
+    @background_image.draw(0, 0, 0);
+  end
+
+  def button_down(id)
+    if id == Gosu::KbEscape
+      close
+    end
   end
 end
 
-window = MyWindow.new
+window = GameWindow.new
 window.show
