3075809125a5632c1eec54382980659ed722bfdc
Matt Todd <matt.c.todd@gmail.com>
2016-01-23 08:05:57 -0600

    background is now in sections of various heights

--- lib/background.rb
+++ lib/background.rb
@@ -5,11 +5,35 @@ require_relative './consts.rb'
 class Background
   def initialize(color = Gosu::Color::GRAY)
     @color = color
+
+    half_height = Consts::WindowHeight / 2
+
+    @sections = [
+      {x: 0, y: half_height},
+      {x: 100, y: half_height + 20},
+      {x: 200, y: half_height + 40},
+      {x: 300, y: half_height + 60},
+      {x: 400, y: half_height + 80},
+    ]
   end
 
   def draw
-    Gosu::draw_rect 0, Consts::WindowHeight / 2, 
-        Consts::WindowWidth, Consts::WindowHeight / 2, 
+    previous_section = nil
+    @sections.each do |section|
+      if previous_section.nil?
+        previous_section = section
+        next
+      end
+
+      Gosu::draw_rect previous_section[:x], previous_section[:y], 
+          section[:x] - previous_section[:x], previous_section[:y],
+          @color, ZOrder::Background
+
+      previous_section = section
+    end
+
+    Gosu::draw_rect previous_section[:x], previous_section[:y], 
+        Consts::WindowWidth - previous_section[:x], previous_section[:y],
         @color, ZOrder::Background
   end
 end
